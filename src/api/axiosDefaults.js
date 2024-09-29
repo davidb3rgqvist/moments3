2183f1d1c38b87f58e3ac5532d03bc2bb6716c10
David Bergqvist <David.bergqvist@gmail.com>
2024-09-29 15:32:56 +0000

    added create posts

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = 'https://drf-be-3512290af8c0.herokuapp.com'
 axios.defaults.headers.post['Content-Type']= 'multipart/form-data'
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
